fe48a621396c01853f95fafc3729c650a55b6a1d
yechanlee00 <doctorcherry.z1@gmail.com>
2026-05-17 15:35:52 +0900

    Create Reduction: Even wider gates.v

---
+++ Verilog Language/More Verilog Features/Reduction: Even wider gates.v
@@ -0,0 +1,12 @@
+module top_module( 
+    input [99:0] in,
+    output out_and,
+    output out_or,
+    output out_xor 
+);
+    
+    assign out_and = &in; //out_and: output of a 100-input AND gate.
+    assign out_or = |in; //out_or: output of a 100-input OR gate.
+    assign out_xor = ^in; //out_xor: output of a 100-input XOR gate.
+
+endmodule
