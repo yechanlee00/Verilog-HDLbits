6cab865511866e6ae1de7ff493e5558377fb4505
yechanlee00 <doctorcherry.z1@gmail.com>
2026-05-22 18:00:47 +0900

    Create DFF with asynchronous reset.v

---
+++ Circuits/Sequential Logic/Latches and Flip-Flops/DFF with asynchronous reset.v
@@ -0,0 +1,16 @@
+module top_module (
+    input clk,
+    input areset,   // active high asynchronous reset
+    input [7:0] d,
+    output reg [7:0] q
+);
+    
+    always @(posedge clk or posedge areset) begin
+        if(areset) begin
+            q <= 8'b00000000;
+        end else begin
+            q <= d;
+        end
+    end
+
+endmodule
